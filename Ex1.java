2cd5f3ba053419cace875daa3b5e5f450eb80166
nayan malviya <naymalviya@gmail.com>
2024-05-24 21:20:00 +0530

    LinkedList examples

---
+++ Ex1.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+/**
+ *   This is Linked list program from collection framework
+ */
+
+class Ex1 {
+    public static void main(String[] args) {
+        // LinkedList as a List
+        LinkedList x = new LinkedList();
+
+        x.add(34);
+        x.add(56);
+        x.add(78);
+        x.add(12);
+        x.add(8);
+
+        System.out.println(x);
+
+        System.out.println(x.remove(0));
+        System.out.println(x.remove(0));
+        System.out.println(x.remove(0));
+        System.out.println(x.remove(0));
+        System.out.println(x.remove(0));
+
+        System.out.println(x);
+    }
+}
